f427eed72a402973d1f9cac9b1e2555cff825b97
Ashish Goyal <goyal.ashish062@gmail.com>
2020-10-05 19:12:34 +0530

    tags vaklidation from product data 8

--- utils/webhookOrdersList.js
+++ utils/webhookOrdersList.js
@@ -6,9 +6,9 @@ let shopifyTagData = async(Object, shop)=>{
   let newArray = []
   Object.line_items.forEach(async(item, i) => {
     let data =await newShopify(shop, item.product_id)
-
+    console.log(data, "product data found");
     if (data.tags!="") {
-      
+      console.log(data.tags, "tags found");
       item.tag = data.tags
       newArray.push(item)
     }
@@ -18,27 +18,27 @@ let shopifyTagData = async(Object, shop)=>{
 
 let saveOrders = async(Object, shop)=>{
 
-let itemData = await shopifyTagData(Object, shop).then(data=>{
-  console.log(data, "then functipn data");
-})
-console.log(itemData);
+let itemData = await shopifyTagData(Object, shop)
+if (itemData.length>0) {
+  console.log(itemData, "itemData");
+}
 
   //Object.line_items.forEach(async (item, i) => {
-    let obj = await {
-                orderId:Object.name,
-                created_at:Object.created_at,
-                first_name: Object.customer.first_name,
-                last_name: Object.customer.last_name,
-
-              };
-
-    const orderData = new orderSchema({
-      order:obj,
-      shop:shop
-    });
-
-    console.log(JSON.stringify(orderData), "stringify data");
-    console.log(orderData, "without stringify");
+    // let obj = await {
+    //             orderId:Object.name,
+    //             created_at:Object.created_at,
+    //             first_name: Object.customer.first_name,
+    //             last_name: Object.customer.last_name,
+    //             item: itemData
+    //           };
+    //
+    // const orderData = new orderSchema({
+    //   order:obj,
+    //   shop:shop
+    // });
+    //
+    // console.log(JSON.stringify(orderData), "stringify data");
+    // console.log(orderData, "without stringify");
     // return await orderData.save(function(err, data){
     //   if (err) {
     //     console.log("save objecft error is", err);
