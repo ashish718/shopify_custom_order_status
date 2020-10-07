63cb947b537a91679b00ffa9606c4f54e7c70384
Ashish Goyal <goyal.ashish062@gmail.com>
2020-10-07 16:47:49 +0530

    tag else render to frontend test case 2

--- utils/webhookOrdersList.js
+++ utils/webhookOrdersList.js
@@ -17,6 +17,10 @@ let shopifyTagData = async(Object, shop)=>{
       console.log(item);
       return item
     }
+    else {
+      item.tag = ""
+      return item
+    }
   });
   // console.log(newArray.length, "length check");
   return await Promise.all(result)
@@ -26,40 +30,36 @@ let saveOrders = async(Object, shop)=>{
 
 let itemData = await shopifyTagData(Object, shop)
 
-if (itemData.length>0) {
-  console.log(itemData, "itemData");
-  //Object.line_items.forEach(async (item, i) => {
-    let obj = await {
-                orderId:Object.name,
-                created_at:Object.created_at,
-                first_name: Object.customer.first_name,
-                last_name: Object.customer.last_name,
-                item: itemData
-              };
+  if (itemData.length>0) {
+    console.log(itemData, "itemData");
+    //Object.line_items.forEach(async (item, i) => {
+      let obj = await {
+                  orderId:Object.name,
+                  created_at:Object.created_at,
+                  first_name: Object.customer.first_name,
+                  last_name: Object.customer.last_name,
+                  item: itemData
+                };
 
-    const orderData = new orderSchema({
-      order:obj,
-      shop:shop
-    });
+      const orderData = new orderSchema({
+        order:obj,
+        shop:shop
+      });
 
-    console.log(JSON.stringify(orderData), "stringify data");
-    console.log(orderData, "without stringify");
-    return await orderData.save(function(err, data){
-      if (err) {
-        console.log("save objecft error is", err);
-      return err;
-      }
-      else {
-        console.log(data, "data saved Successfully");
-        return data;
-      }
+      console.log(JSON.stringify(orderData), "stringify data");
+      console.log(orderData, "without stringify");
+      return await orderData.save(function(err, data){
+        if (err) {
+          console.log("save objecft error is", err);
+        return err;
+        }
+        else {
+          console.log(data, "data saved Successfully");
+          return data;
+        }
 
-    });
-}
-
-
-
-  //})
+      });
+  }
 }
 
 module.exports.saveOrders = saveOrders
