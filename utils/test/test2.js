ccaabbc0845c3d35e824c9f4f653f7abdf558db2
Ashish Goyal <goyal.ashish062@gmail.com>
2020-10-22 15:07:19 +0530

    console in update status

--- utils/test/test2.js
+++ utils/test/test2.js
@@ -2,10 +2,10 @@ let msg91 = require("msg91")("129310AeIewjfm5d19a1bb", "BEWELL", "ROUTE_NO" );
 
 
 
-module.exports.sms = async(docs, shop, data)=>{
-console.log(docs, "docs");
-console.log(shop, "shop");
-console.log(data, "data");
+module.exports.sms = async(docs, shop)=>{
+// console.log(docs, "docs");
+// console.log(shop, "shop");
+// console.log(data, "data");
   // let message = `Hi greetings from ${shop} your ${obj.orderId} status is ${obj.status}`
   //
   // return await msg91.send(mobileNo, message, function(err, response){
