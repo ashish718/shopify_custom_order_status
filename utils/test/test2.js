2b5c178358553b80004eeb212abed4abe531f8ac
Ashish Goyal <goyal.ashish062@gmail.com>
2020-10-22 15:50:02 +0530

    sms testing 3

--- utils/test/test2.js
+++ utils/test/test2.js
@@ -6,10 +6,13 @@ module.exports.sms = async(mobileNo, obj, shop)=>{
 // console.log(docs, "docs");
 // console.log(shop, "shop");
 // console.log(data, "data");
+console.log(mobileNo, "mobileNo");
+
   let message = `Hi greetings from ${shop} your ${obj.orderId} of item ${obj.item.name} status is ${obj.status}`
 
   return await msg91.send(mobileNo, message, function(err, response){
       if (err) return console.log(err);
+      console.log(response, "response");
       return response
   });
 }
